8da3ee0982b7287bd722620eddb4d59078a208b0
reabel <ryanreabel@gmail.com>
2023-10-28 15:16:29 -0400

    Default code configuration based on MUI docs

--- Javascript/react/sandbox/recipe/src/main.tsx
+++ Javascript/react/sandbox/recipe/src/main.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from './App.tsx'
 import './index.css'
 import Layout from "./pages/Layout";
-// import Home from "./pages/Home";
+import Home from "./pages/Home";
 import Blogs from "./pages/Blogs";
 import Contact from "./pages/Contact";
 import NoPage from "./pages/NoPage";
@@ -14,13 +13,12 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-          {/* <Route index element={<Home />} /> */}
+          <Route index element={<Home />} />
           <Route path="blogs" element={<Blogs />} />
           <Route path="contact" element={<Contact />} />
           <Route path="*" element={<NoPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
-    {/* <App /> */}
   </React.StrictMode>,
 )
