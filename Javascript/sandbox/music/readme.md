f673f93a74c5f90f9e55824e53096e9601b00f7f
reabel <ryanreabel@gmail.com>
2024-04-30 11:20:30 -0400

    Work on KB Reader API, note on MDN Web Audio API article

--- Javascript/sandbox/music/readme.md
+++ Javascript/sandbox/music/readme.md
@@ -5,6 +5,7 @@
 ### Browser Based
 
 - [ToneJS](https://tonejs.github.io/)
+- [MDN article on Web Audio API](https://developer.mozilla.org/en-US/docs/Web/API/Web_Audio_API)
 
 ### Terminal Based
 
--- kb-reader-api/server.js
+++ kb-reader-api/server.js
@@ -1,42 +1,51 @@
+/**
+ * KB Reader API 2024 - Ryan Reabel
+ * 
+ * TODO: Sanitize input from request:
+ *   - ensure only scope of internal project is allowed.
+ *   - ensure only markdown files are retrieved.
+ *   - ensure yaml spec isn't impacted by these changes.
+ * TODO: General error handling
+ *   - File not found.
+ * TODO: MD file structure retrieval
+ */
 import * as fs from "fs";
 import express from "express";
 const app = express();
 const port = 8000;
 
-//const test = path.resolve("../podcasts.md");
 const test = "podcasts.md";
-//Don't fetch, juts read from FS and return text, duh.
-const testFetch = () => {
-  return fs.readFileSync(test);
-};
 
+/**
+ * fetchMD returns text from markdown files
+ * TODO: Check integrity of
+ * @returns Markdown text
+ */
 const lookup = (path) => {
   console.log("looking up ", path);
   return fs.readFileSync("../" + path);
 };
 
+const analyzeStruct = () => {
+  return "WIP";
+};
+
 app.get("/", (req, res) => {
-  res.send("Main - Structure");
+  res.send("**KB Reader API - WIP** \n Routes available: \n /markdown - query for md files \n /structure - returns a list of queryable markdown files \n /spec - returns API info in YAML form");
 });
 
-app.get("/test/:route", (req, res) => {
-  console.log("test w/ route", req.params, req.body);
-  res.send(lookup(req.params["route"] || test));
-});
-
-app.get("/test", (req, res) => {
-  console.log("test w/o route", req.params, req.body, req.query.route);
-  res.send(lookup(req.query.route || test));
+app.get("/spec", (req, res) => {
+  res.send(lookup("./kb-reader-api/spec.yaml"));
 });
 
 app.get("/structure", (req, res) => {
-  res.send(testFetch());
+  res.send(analyzeStruct());
 });
 
 app.get("/markdown", (req, res) => {
-  res.send("Markdown");
+  res.send(lookup(req.query.route || test));
 });
 
 app.listen(port, () => {
-  console.log("We are live on " + port);
+  console.log("Server active on " + port + " Routes available: \n /markdown - query for md files \n /structure - returns a list of queryable markdown files \n /spec - returns API info in YAML form");
 });
