2209a3b9a3ce6224233b25ab62b58e53daa6e3dd
reabel <ryanreabel@gmail.com>
2024-07-21 22:30:10 -0400

    init store-24

---
+++ Javascript/vue/sandbox/store-prototype/src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+    <header>
+        <nav>
+            <RouterLink to="/">Home</RouterLink>
+            <RouterLink to="/about">About</RouterLink>
+            <RouterLink to="/browse">Browse</RouterLink>
+        </nav>
+    </header>
+
+    <RouterView />
+</template>
+
+<style scoped>
+
+</style>
---
+++ Javascript/vue/sandbox/store-prototype/src/views/BrowseView.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="about">
+      <h1>Items for sale</h1>
+      <table>
+        <thead>
+            <tr>
+                <th>Item</th>
+                <th>Price</th>
+            </tr>
+        </thead>
+        <body>
+            <tr>
+                <td>Shoes</td>
+                <td>$50</td>
+            </tr>
+            <tr>
+                <td>Shirt</td>
+                <td>$25</td>
+            </tr>
+            <tr>
+                <td>Pants</td>
+                <td>$40</td>
+            </tr>
+        </body>
+      </table>
+    </div>
+  </template>
+  
+  <style>
+  @media (min-width: 1024px) {
+    .about {
+      min-height: 100vh;
+      display: flex;
+      align-items: center;
+    }
+  }
+  </style>
+  
